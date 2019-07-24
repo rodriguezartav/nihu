81f3edf4e66379c153455afc2509486343d11eae
Roberto Rodriguez <roberto@rodriguezartavia.com>
2019-07-23 22:08:26 -0600

    text and descriptions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import Header from "./components/header";
