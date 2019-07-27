29528e2270293d8d968a2d1f8b95724cb0337659
Roberto Rodriguez <roberto@rodriguezartavia.com>
2019-07-27 16:59:34 -0600

    password

--- src/App.js
+++ src/App.js
@@ -12,33 +12,39 @@ import Team from "./views/team";
 import Project from "./views/project";
 import ScrollToTop from "./helpers/scrollTo";
 function App() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const nihu = urlParams.get("nihu");
-  if (!nihu)
+  const [value, setValue] = React.useState(process.env.REACT_APP_ENV === "production" ? 2 : "");
+
+  function onChange(e) {
+    setValue(parseInt(e.currentTarget.value));
+  }
+
+  if (value === 2)
     return (
-      <div>
-        <h1>Agregue ?nihu=true a la dirección para accesar el sitio.</h1>
+      <BrowserRouter>
+        <ScrollToTop>
+          <div className="App">
+            <Header />
+            <div className="header-space" />
+            <Route path="/" exact component={Home} />
+            <Route path="/index.html" exact component={Home} />
+            <Route path="/about-us.html/" component={About} />
+            <Route path="/contact-us.html/" component={Contact} />
+            <Route path="/team.html/" component={Team} />
+            <Route path="/proyectos/:id" component={Project} />
+            <Route path="/proyectos/:id/:image" component={Project} />
+            <Footer />
+          </div>
+        </ScrollToTop>
+      </BrowserRouter>
+    );
+  else
+    return (
+      <div style={{ margin: 10 }}>
+        <h1>Acceso restringido</h1>
+        <input placeholder="Cuanto es 2 + 2?" onChange={onChange} value={value} />
         <p>Porque se hace esto? Para que no aparezca en Google Search.</p>
       </div>
     );
-  return (
-    <BrowserRouter>
-      <ScrollToTop>
-        <div className="App">
-          <Header />
-          <div className="header-space" />
-          <Route path="/" exact component={Home} />
-          <Route path="/index.html" exact component={Home} />
-          <Route path="/about-us.html/" component={About} />
-          <Route path="/contact-us.html/" component={Contact} />
-          <Route path="/team.html/" component={Team} />
-          <Route path="/proyectos/:id" component={Project} />
-          <Route path="/proyectos/:id/:image" component={Project} />
-          <Footer />
-        </div>
-      </ScrollToTop>
-    </BrowserRouter>
-  );
 }
 
 export default App;
